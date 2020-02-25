9e5cb22f366121c866914acc46650b836a694c91
uhooi <uhooi@server.fake>
2020-02-25 11:58:54 +0900

    Create Xcode project

---
+++ UhooiPicBookTests/UhooiPicBookTests.swift
@@ -0,0 +1,34 @@
+//
+//  UhooiPicBookTests.swift
+//  UhooiPicBookTests
+//
+//  Created by uhooi on 2020/02/24.
+//  Copyright © 2020 THE Uhooi. All rights reserved.
+//
+
+import XCTest
+@testable import UhooiPicBook
+
+class UhooiPicBookTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
