d3bcd511a53fc0f1395f99e27b9414eeec9a09ef
uhooi <uhooi@server.fake>
2020-03-01 15:57:09 +0900

    Add MonsterListPresenter unit test

--- UhooiPicBookTests/Modules/MonsterList/Presenters/MonsterListPresenterTests.swift
+++ UhooiPicBookTests/Modules/MonsterList/Presenters/MonsterListPresenterTests.swift
@@ -37,10 +37,48 @@ final class MonsterListPresenterTests: XCTestCase {
     func test_viewDidLoad() {
         self.presenter.viewDidLoad()
 
-        // TODO: Assert
+        XCTAssertEqual(self.viewMock.startIndicatorCallCount, 1)
+        XCTAssertEqual(self.interactorMock.fetchMonstersCallCount, 1)
     }
 
     // MARK: MonsterListInteractorOutput
+    
+    // MARK: monstersFetched()
+    
+    func test_monstersFetched_zero() {
+        let monsterDTOs: [MonsterDTO] = []
+        self.viewMock.showMonstersHandler = { monsters in
+            for index in 0 ..< monsterDTOs.count {
+                XCTAssertEqual(monsters[index].icon, monsterDTOs[index].icon)
+                XCTAssertEqual(monsters[index].name, monsterDTOs[index].name)
+                XCTAssertEqual(monsters[index].description, monsterDTOs[index].description)
+            }
+        }
+        
+        self.presenter.monstersFetched(monsters: monsterDTOs)
+        
+        XCTAssertEqual(self.viewMock.showMonstersCallCount, 1)
+        XCTAssertEqual(self.viewMock.stopIndicatorCallCount, 1)
+    }
+    
+    func test_monstersFetched_three() {
+        let uhooiDTO = MonsterDTO(icon: UIImage(), name: "uhooi", description: "uhooi's description", order: 1)
+        let ayausaDTO = MonsterDTO(icon: UIImage(), name: "ayausa", description: "ayausa's description", order: 2)
+        let chibirdDTO = MonsterDTO(icon: UIImage(), name: "chibird", description: "chibird's description", order: 3)
+        let monsterDTOs = [uhooiDTO, ayausaDTO, chibirdDTO]
+        self.viewMock.showMonstersHandler = { monsters in
+            for index in 0 ..< monsterDTOs.count {
+                XCTAssertEqual(monsters[index].icon, monsterDTOs[index].icon)
+                XCTAssertEqual(monsters[index].name, monsterDTOs[index].name)
+                XCTAssertEqual(monsters[index].description, monsterDTOs[index].description)
+            }
+        }
+        
+        self.presenter.monstersFetched(monsters: monsterDTOs)
+        
+        XCTAssertEqual(self.viewMock.showMonstersCallCount, 1)
+        XCTAssertEqual(self.viewMock.stopIndicatorCallCount, 1)
+    }
 
     // MARK: - Other Private Methods
 
