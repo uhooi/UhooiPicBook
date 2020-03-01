6291e0c62677fb2ae8e473286b8b9cb902e1b26e
uhooi <uhooi@server.fake>
2020-03-01 12:23:40 +0900

    Fail to make cells into cards

--- UhooiPicBook/Modules/MonsterList/Views/MonsterCollectionViewCell.swift
+++ UhooiPicBook/Modules/MonsterList/Views/MonsterCollectionViewCell.swift
@@ -9,6 +9,40 @@ import UIKit
 
 final class MonsterCollectionViewCell: UICollectionViewCell {
 
+    // MARK: Stored Instance Properties
+
+    @IBInspectable private var masksToBounds: Bool = false {
+        willSet {
+            self.layer.masksToBounds = newValue
+            //            self.contentView.layer.masksToBounds = newValue
+        }
+    }
+
+    @IBInspectable private var cornerRadius: CGFloat = 0.0 {
+        willSet {
+            self.layer.cornerRadius = newValue
+            //            self.contentView.layer.cornerRadius = newValue
+        }
+    }
+
+    @IBInspectable private var shadowOffset: CGSize = CGSize(width: 0.0, height: 0.0) {
+        willSet {
+            self.layer.shadowOffset = newValue
+        }
+    }
+
+    @IBInspectable private var shadowRadius: CGFloat = 0.0 {
+        willSet {
+            self.layer.shadowRadius = newValue
+        }
+    }
+
+    @IBInspectable private var shadowOpacity: Float = 0.0 {
+        willSet {
+            self.layer.shadowOpacity = newValue
+        }
+    }
+
     // MARK: IBOutlets
 
     @IBOutlet private weak var iconImageView: UIImageView!
