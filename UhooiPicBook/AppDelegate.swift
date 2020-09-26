d84e790497a708b6ff2c6451c7d477948a4f5df8
uhooi <uhooi@server.fake>
2020-09-26 12:55:36 +0900

    Fix notification handler

--- UhooiPicBook/AppDelegate.swift
+++ UhooiPicBook/AppDelegate.swift
@@ -62,7 +62,11 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
         _ center: UNUserNotificationCenter,
         willPresent notification: UNNotification,
         withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([[.alert, .sound]])
+        if #available(iOS 14.0, *) {
+            completionHandler([[.banner, .list, .sound]])
+        } else {
+            completionHandler([[.alert, .sound]])
+        }
     }
 
     func userNotificationCenter(
