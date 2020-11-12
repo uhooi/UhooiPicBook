3951f73d63f5ae77b56f52c6e44b5b8799bb2010
uhooi <uhooi@server.fake>
2020-11-12 20:37:07 +0900

    Refactor loadMonsters

--- Shared/Repository/Monsters/MonstersRepository.swift
+++ Shared/Repository/Monsters/MonstersRepository.swift
@@ -17,37 +17,36 @@ final class MonstersFirebaseClient {
 }
 
 extension MonstersFirebaseClient: MonstersRepository {
-
+    
     func loadMonsters(_ completion: @escaping (Result<[MonsterDTO], Error>) -> Void) {
-        var monsters: [MonsterDTO] = []
-        var someError: Error?
-
         let monstersRef = self.firestore.collection("monsters")
         monstersRef.getDocuments { querySnapshot, error in
             if let error = error {
-                someError = error
-            } else {
-                guard let querySnapshot = querySnapshot else {
-                    fatalError("Fail to unwrap `querySnapshot` .")
-                }
-
-                for document in querySnapshot.documents.filter({ $0.exists }) {
-                    let monster = document.data()
-                    guard let name = monster["name"] as? String,
-                          let description = monster["description"] as? String,
-                          let baseColorCode = monster["base_color"] as? String,
-                          let iconUrlString = monster["icon_url"] as? String,
-                          let dancingUrlString = monster["dancing_url"] as? String,
-                          let order = monster["order"] as? Int else {
-                        continue
-                    }
-
-                    monsters.append(MonsterDTO(name: name, description: description, baseColorCode: baseColorCode, iconUrlString: iconUrlString, dancingUrlString: dancingUrlString, order: order))
-                }
+                completion(.failure(error))
+                return
             }
-
-            completion(someError.map(Result.failure) ?? .success(monsters))
+            
+            guard let querySnapshot = querySnapshot else {
+                fatalError("Fail to unwrap `querySnapshot` .")
+            }
+            
+            var monsters: [MonsterDTO] = []
+            for document in querySnapshot.documents.filter({ $0.exists }) {
+                let monster = document.data()
+                guard let name = monster["name"] as? String,
+                      let description = monster["description"] as? String,
+                      let baseColorCode = monster["base_color"] as? String,
+                      let iconUrlString = monster["icon_url"] as? String,
+                      let dancingUrlString = monster["dancing_url"] as? String,
+                      let order = monster["order"] as? Int else {
+                    continue
+                }
+                
+                monsters.append(MonsterDTO(name: name, description: description, baseColorCode: baseColorCode, iconUrlString: iconUrlString, dancingUrlString: dancingUrlString, order: order))
+            }
+            
+            completion(.success(monsters))
         }
     }
-
+    
 }
