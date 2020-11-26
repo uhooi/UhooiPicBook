435f92441bc73653228082db638724aa204225c6
uhooi <uhooi@server.fake>
2020-11-26 17:30:26 +0900

    Add --mock-final

--- Scripts/XcodeGen/mockolo.sh
+++ Scripts/XcodeGen/mockolo.sh
@@ -1,6 +1,6 @@
 if which mint >/dev/null; then
   rm -f $SRCROOT/$TARGET_NAME/Generated/MockResults.swift
-  xcrun --sdk macosx mint run uber/mockolo mockolo --sourcedirs $SRCROOT/$TARGET_NAME --destination $SRCROOT/$TARGET_NAME/Generated/MockResults.swift
+  xcrun --sdk macosx mint run uber/mockolo mockolo --sourcedirs $SRCROOT/$TARGET_NAME --destination $SRCROOT/$TARGET_NAME/Generated/MockResults.swift --mock-final
 else
   echo "warning: Mint not installed, download from https://github.com/yonaskolb/Mint"
 fi
