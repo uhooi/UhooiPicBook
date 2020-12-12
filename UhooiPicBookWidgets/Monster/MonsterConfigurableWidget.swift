c28fa7f93b7022b23adaef12310fb52d830295ee
takehito koshimizu <30564669+Koshimizu-Takehito@users.noreply.github.com>
2020-12-12 16:40:40 +0900

    Add support medium family
    Co-authored-by: uhooi <the_uhooi@yahoo.co.jp>

--- UhooiPicBookWidgets/Monster/MonsterConfigurableWidget.swift
+++ UhooiPicBookWidgets/Monster/MonsterConfigurableWidget.swift
@@ -31,7 +31,7 @@ struct MonsterConfigurableWidget: Widget {
         )
         .configurationDisplayName("configurationDisplayName")
         .description("configurableDescription")
-        .supportedFamilies([.systemSmall])
+        .supportedFamilies([.systemSmall, .systemMedium])
     }
 }
 
