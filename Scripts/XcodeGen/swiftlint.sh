96ae3e1c72a0d0308dd230d91ebe3032ec6964da
uhooi <uhooi@server.fake>
2021-04-02 22:42:04 +0900

    Revert "Add echo for debug"
    This reverts commit d91c0abaf7dd58d46acabde7cc9e67e2634e9e02.

--- Scripts/XcodeGen/swiftlint.sh
+++ Scripts/XcodeGen/swiftlint.sh
@@ -1,6 +1,3 @@
-// FIXME: `echo` は後で削除する
-echo $MINT_PATH
-echo $MINT_LINK_PATH
 if which mint >/dev/null; then
   xcrun --sdk macosx mint run swiftlint swiftlint autocorrect --format
   xcrun --sdk macosx mint run swiftlint swiftlint
