fae53490eff8e786a23cdac38286eb9023f3d91f
uhooi <uhooi@server.fake>
2021-04-29 15:56:07 +0900

    Refactor indent

--- Scripts/XcodeGen/mockolo.sh
+++ Scripts/XcodeGen/mockolo.sh
@@ -2,7 +2,7 @@ export MINT_PATH=mint/lib
 export MINT_LINK_PATH=mint/bin
 if which mint >/dev/null; then
   rm -f $SRCROOT/$TARGET_NAME/Generated/MockResults.swift
-xcrun --sdk macosx mint run uber/mockolo mockolo --sourcedirs $SRCROOT/$TARGET_NAME $SRCROOT/Shared --destination $SRCROOT/$TARGET_NAME/Generated/MockResults.swift --mock-final
+  xcrun --sdk macosx mint run uber/mockolo mockolo --sourcedirs $SRCROOT/$TARGET_NAME $SRCROOT/Shared --destination $SRCROOT/$TARGET_NAME/Generated/MockResults.swift --mock-final
 else
   echo "warning: Mint not installed, download from https://github.com/yonaskolb/Mint"
 fi
