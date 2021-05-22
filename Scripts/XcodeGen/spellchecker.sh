c54bce11adbaa6820a8f22d08b72148f920a169a
uhooi <uhooi@server.fake>
2021-05-22 23:39:05 +0900

    Manage SpellChecker with SwiftPM instead of Mint

--- Scripts/XcodeGen/spellchecker.sh
+++ Scripts/XcodeGen/spellchecker.sh
@@ -1,10 +1,3 @@
-export MINT_PATH=mint/lib
-export MINT_LINK_PATH=mint/bin
-if ! which mint >/dev/null; then
-  echo "warning: Mint not installed, download from https://github.com/yonaskolb/Mint"
-  exit 0
-fi
-
 git_path=/usr/local/bin/git
 files=$($git_path diff --diff-filter=d --name-only -- "*.swift" "*.h" "*.m")
 if (test -z $files) || (test ${#files[@]} -eq 0); then
@@ -18,5 +11,6 @@ do
   options="$options $SRCROOT/$file"
 done
 
-xcrun --sdk macosx mint run fromkk/SpellChecker SpellChecker --yml $SRCROOT/spell-checker.yml -- $options
+SDKROOT=macosx
+swift run -c release --package-path BuildTools SpellChecker --yml $SRCROOT/spell-checker.yml -- $options
 
