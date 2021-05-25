284a25dfccf3f810b747316c60b82f2e810f2895
uhooi <uhooi@server.fake>
2021-05-25 23:18:37 +0900

    Bump IBLinter to 0.4.27

--- Tools/UhooiPicBookIBLinter/Package.swift
+++ Tools/UhooiPicBookIBLinter/Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     dependencies: [
         // TODO: The target name will no longer conflict with XcodeGen in the next release,
         // and then we'll put the package together.
-        .package(url: "https://github.com/IBDecodable/IBLinter", .exact("0.4.25")),
+        .package(url: "https://github.com/IBDecodable/IBLinter", .exact("0.4.27")),
     ],
     targets: [.target(name: "UhooiPicBookIBLinter", path: "")]
 )
