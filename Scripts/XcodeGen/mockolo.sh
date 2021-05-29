6c0d7aeb789863082f40c67f5a1dcdbb6e91ba64
uhooi <uhooi@server.fake>
2021-05-29 17:25:30 +0900

    Fix Mockolo

--- Scripts/XcodeGen/mockolo.sh
+++ Scripts/XcodeGen/mockolo.sh
@@ -1,4 +1,5 @@
 rm -f $SRCROOT/$TARGET_NAME/Generated/MockResults.swift
 SDKROOT=macosx
-swift run -c release --package-path Tools/UhooiPicBookMockolo mockolo --sourcedirs $SRCROOT/$TARGET_NAME $SRCROOT/Shared --destination $SRCROOT/$TARGET_NAME/Generated/MockResults.swift --mock-final
+swift build -c release --package-path Tools/UhooiPicBookMockolo --product mockolo
+Tools/UhooiPicBookMockolo/.build/release/mockolo --sourcedirs $SRCROOT/$TARGET_NAME $SRCROOT/Shared --destination $SRCROOT/$TARGET_NAME/Generated/MockResults.swift --mock-final
 
