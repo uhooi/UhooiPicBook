252260e28ac6aead4f67a6ddab26e2f2059e53ed
uhooi <the_uhooi@yahoo.co.jp>
2021-10-08 21:50:11 +0900

    Fix Mockolo error

--- Scripts/XcodeGen/mockolo.sh
+++ Scripts/XcodeGen/mockolo.sh
@@ -1,4 +1,4 @@
 rm -f $SRCROOT/$TARGET_NAME/Generated/MockResults.swift
 SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
-Tools/UhooiPicBookMockolo/.build/release/mockolo --sourcedirs $SRCROOT/$TARGET_NAME $SRCROOT/Shared --destination $SRCROOT/$TARGET_NAME/Generated/MockResults.swift --mock-final
+Tools/UhooiPicBookMockolo/.build/release/mockolo --sourcedirs $SRCROOT/$TARGET_NAME --sourcedirs $SRCROOT/Shared --destination $SRCROOT/$TARGET_NAME/Generated/MockResults.swift --mock-final
 
