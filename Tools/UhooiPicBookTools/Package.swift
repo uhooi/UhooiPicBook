48da169560bdfd44947fc48ec68e31bc86072c45
uhooi <the_uhooi@yahoo.co.jp>
2021-10-22 21:40:25 +0900

    Bump SwiftLint to 0.44.0

--- Tools/UhooiPicBookTools/Package.swift
+++ Tools/UhooiPicBookTools/Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/yonaskolb/XcodeGen", .exact("2.25.0")),
-        .package(url: "https://github.com/realm/SwiftLint", .exact("0.43.1")),
+        .package(url: "https://github.com/realm/SwiftLint", .exact("0.44.0")),
         .package(url: "https://github.com/IBDecodable/IBLinter", .exact("0.4.27")),
         .package(url: "https://github.com/fromkk/SpellChecker", .exact("0.1.0")),
         // .package(url: "https://github.com/uber/mockolo", .exact("1.6.1")),
