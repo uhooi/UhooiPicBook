11de4c4947edd9aa5b5e674a07c362fe7082bf3f
uhooi <the_uhooi@yahoo.co.jp>
2021-12-16 14:28:58 +0900

    Uncheck Sendable

---
+++ Shared/Extensions/UncheckedSendable.swift
@@ -0,0 +1,13 @@
+// swiftlint:disable:this file_name
+//
+//  UncheckedSendable.swift
+//  UhooiPicBook
+//
+//  Created by uhooi on 2021/12/16.
+//
+
+import UIKit
+
+extension URL: @unchecked Sendable {}
+extension IndexPath: @unchecked Sendable {}
+extension UIImage: @unchecked Sendable {}
