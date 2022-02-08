1b226766aea9cd4d237dd3a2f2a9783e979537bc
uhooi <the_uhooi@yahoo.co.jp>
2022-02-09 00:28:55 +0900

    Bump Mockolo from 1.6.2 to 1.6.3

--- Tools/UhooiPicBookTools/Package.swift
+++ Tools/UhooiPicBookTools/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .package(url: "https://github.com/realm/SwiftLint", .exact("0.45.0")),
         .package(url: "https://github.com/IBDecodable/IBLinter", .exact("0.4.27")),
         .package(url: "https://github.com/fromkk/SpellChecker", .exact("0.1.0")),
-        .package(url: "https://github.com/uber/mockolo", .exact("1.6.2")),
+        .package(url: "https://github.com/uber/mockolo", .exact("1.6.3")),
         .package(url: "https://github.com/mono0926/LicensePlist", .exact("3.17.0")),
         .package(url: "https://github.com/tuist/xcbeautify", .exact("0.11.0")),
     ],
