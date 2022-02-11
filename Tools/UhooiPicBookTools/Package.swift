2fe051a362fd45a6f1d6b009dcd09e2e255682a5
uhooi <the_uhooi@yahoo.co.jp>
2022-02-11 14:51:33 +0900

    Bump IBLinter from 0.4.27 to 0.5.0

--- Tools/UhooiPicBookTools/Package.swift
+++ Tools/UhooiPicBookTools/Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/realm/SwiftLint", .exact("0.45.0")),
-        .package(url: "https://github.com/IBDecodable/IBLinter", .exact("0.4.27")),
+        .package(url: "https://github.com/IBDecodable/IBLinter", .exact("0.5.0")),
         .package(url: "https://github.com/fromkk/SpellChecker", .exact("0.1.0")),
         .package(url: "https://github.com/uber/mockolo", .exact("1.6.3")),
         .package(url: "https://github.com/mono0926/LicensePlist", .exact("3.17.0")),
