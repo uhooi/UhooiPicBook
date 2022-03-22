ced59a34bbe6e44bfe765b90453f571d2fc8ba11
uhooi <the_uhooi@yahoo.co.jp>
2022-03-22 22:20:47 +0900

    Bump SwiftPM from 5.5 to 5.6

--- Tools/UhooiPicBookTools/Package.swift
+++ Tools/UhooiPicBookTools/Package.swift
@@ -1,19 +1,19 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 
 import PackageDescription
 
 let package = Package(
     name: "UhooiPicBookTools",
     platforms: [
-        .macOS(.v11),
+        .macOS(.v12),
     ],
     dependencies: [
-        .package(url: "https://github.com/realm/SwiftLint", .exact("0.45.0")),
-        .package(url: "https://github.com/IBDecodable/IBLinter", .exact("0.5.0")),
-        .package(url: "https://github.com/fromkk/SpellChecker", .exact("0.1.0")),
-        .package(url: "https://github.com/uber/mockolo", .exact("1.6.3")),
-        .package(url: "https://github.com/mono0926/LicensePlist", .exact("3.19.1")),
-        .package(url: "https://github.com/tuist/xcbeautify", .exact("0.11.0")),
+        .package(url: "https://github.com/realm/SwiftLint", exact: "0.45.0"),
+        .package(url: "https://github.com/IBDecodable/IBLinter", exact: "0.5.0"),
+        .package(url: "https://github.com/fromkk/SpellChecker", exact: "0.1.0"),
+        .package(url: "https://github.com/uber/mockolo", exact: "1.6.3"),
+        .package(url: "https://github.com/mono0926/LicensePlist", exact: "3.19.1"),
+        .package(url: "https://github.com/tuist/xcbeautify", exact: "0.11.0"),
     ],
     targets: [.target(name: "UhooiPicBookTools", path: "")]
 )
