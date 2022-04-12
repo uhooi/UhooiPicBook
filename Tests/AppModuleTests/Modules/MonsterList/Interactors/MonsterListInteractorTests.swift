e47160e6e62539f02654428e1c77242f5ea84753
uhooi <the_uhooi@yahoo.co.jp>
2022-04-12 22:22:37 +0900

    Fix unit tests

--- Tests/AppModuleTests/Modules/MonsterList/Interactors/MonsterListInteractorTests.swift
+++ Tests/AppModuleTests/Modules/MonsterList/Interactors/MonsterListInteractorTests.swift
@@ -18,7 +18,7 @@ final class MonsterListInteractorTests: XCTestCase {
     private var monstersRepositoryMock: MonstersRepositoryMock!
     private var monstersTempRepositoryMock: MonstersTempRepositoryMock!
     private var spotlightRepositoryMock: SpotlightRepositoryMock!
-    private var interactor: MonsterListInteractor!
+    private var interactor: MonsterListInteractor<SpotlightRepositoryMock, MonstersRepositoryMock, MonstersTempRepositoryMock>!
 
     // MARK: TestCase Life-Cycle Methods
 
