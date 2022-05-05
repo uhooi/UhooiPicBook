9521389b88d4bc1a5ed0f7cc8b3413924d515a77
uhooi <the_uhooi@yahoo.co.jp>
2022-05-05 19:33:09 +0900

    Update IBLinter

--- Tools/UhooiPicBookTools/Package.swift
+++ Tools/UhooiPicBookTools/Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/realm/SwiftLint", exact: "0.47.1"),
-        .package(url: "https://github.com/uhooi/IBLinter", exact: "0.6.0"),
+        .package(url: "https://github.com/IBDecodable/IBLinter", exact: "0.5.0"),
         .package(url: "https://github.com/fromkk/SpellChecker", exact: "0.1.0"),
         .package(url: "https://github.com/uber/mockolo", exact: "1.7.0"),
         .package(url: "https://github.com/mono0926/LicensePlist", exact: "3.19.1"),
