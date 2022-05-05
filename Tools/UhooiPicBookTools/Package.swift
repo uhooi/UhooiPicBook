f8d0c1f18630d95d10e32ded0caa8ba33f810f75
uhooi <the_uhooi@yahoo.co.jp>
2022-05-05 17:26:05 +0900

    Bump SwiftLint from 0.47.0 to 0.47.1

--- Tools/UhooiPicBookTools/Package.swift
+++ Tools/UhooiPicBookTools/Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .macOS(.v12),
     ],
     dependencies: [
-        .package(url: "https://github.com/realm/SwiftLint", exact: "0.47.0"),
+        .package(url: "https://github.com/realm/SwiftLint", exact: "0.47.1"),
         .package(url: "https://github.com/uhooi/IBLinter", exact: "0.6.0"),
         .package(url: "https://github.com/fromkk/SpellChecker", exact: "0.1.0"),
         .package(url: "https://github.com/uber/mockolo", exact: "1.7.0"),
