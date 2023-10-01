398089eeed8aa67f113045a9a6179a80f4089e4a
uhooi <the_uhooi@yahoo.co.jp>
2023-10-01 14:22:45 +0900

    Update MonsterDetailPresenterTests

--- Tests/AppModuleTests/Modules/MonsterDetail/Presenters/MonsterDetailPresenterTests.swift
+++ Tests/AppModuleTests/Modules/MonsterDetail/Presenters/MonsterDetailPresenterTests.swift
@@ -7,96 +7,28 @@
 //
 
 import XCTest
+import Testing
 @testable import AppModule
 
 @MainActor
-final class MonsterDetailPresenterTests: XCTestCase {
+struct MonsterDetailPresenterTests {
 
     // MARK: Stored Instance Properties
 
-    private var viewMock: MonsterDetailUserInterfaceMock!
-    private var interactorMock: MonsterDetailInteractorInputMock!
-    private var routerMock: MonsterDetailRouterInputMock!
+    private let viewMock: MonsterDetailUserInterfaceMock
+    private let interactorMock: MonsterDetailInteractorInputMock
+    private let routerMock: MonsterDetailRouterInputMock
 
-    private var presenter: MonsterDetailPresenter<
+    private let presenter: MonsterDetailPresenter<
         MonsterDetailUserInterfaceMock,
         MonsterDetailInteractorInputMock,
         MonsterDetailRouterInputMock
-    >!
+    >
 
     // MARK: TestCase Life-Cycle Methods
 
     @MainActor
-    override func setUpWithError() throws {
-        reset()
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    // MARK: - Test Methods
-
-    // MARK: MonsterDetailEventHandler
-
-    // MARK: viewDidLoad()
-
-    func test_viewDidLoad() {
-        presenter.viewDidLoad()
-    }
-    
-    // MARK: didTapDancingImageView()
-    
-    func test_didTapDancingImageView_notNil() {
-        presenter.didTapDancingImageView(dancingImage: UIImage())
-        
-        XCTAssertEqual(routerMock.popupDancingImageCallCount, 1)
-    }
-    
-    func test_didTapDancingImageView_nil() {
-        presenter.didTapDancingImageView(dancingImage: nil)
-        
-        XCTAssertEqual(routerMock.popupDancingImageCallCount, 0)
-    }
-    
-    // MARK: didTapShareButton()
-    
-    func test_didTapShareButton_one_nil() {
-        typealias TestCase = (senderView: UIView?, name: String?, description: String?, icon: UIImage?, line: UInt)
-        let testCases: [TestCase] = [
-            (nil, "name", "description", UIImage(), #line),
-            (UIView(), nil, "description", UIImage(), #line),
-            (UIView(), "name", nil, UIImage(), #line),
-            (UIView(), "name", "description", nil, #line)
-        ]
-        
-        for (senderView, name, description, icon, line) in testCases {
-            presenter.didTapShareButton(senderView, name: name, description: description, icon: icon)
-            XCTAssertEqual(routerMock.showActivityCallCount, 0, line: line)
-        }
-    }
-    
-    func test_didTapShareButton_all_notNil() {
-        let senderView = UIView()
-        let name = "name"
-        let description = "description"
-        let icon = UIImage()
-        routerMock.showActivityHandler = { sourceView, text, image in
-            XCTAssertEqual(text, "\(name)\n\(description)\n#UhooiPicBook")
-            XCTAssertEqual(sourceView, senderView)
-            XCTAssertEqual(image, icon)
-        }
-        
-        presenter.didTapShareButton(senderView, name: name, description: description, icon: icon)
-
-        XCTAssertEqual(routerMock.showActivityCallCount, 1)
-    }
-
-    // MARK: MonsterDetailInteractorOutput
-
-    // MARK: - Other Private Methods
-
-    private func reset() {
+    init() {
         self.viewMock = MonsterDetailUserInterfaceMock()
         self.interactorMock = MonsterDetailInteractorInputMock()
         self.routerMock = MonsterDetailRouterInputMock()
@@ -106,4 +38,71 @@ final class MonsterDetailPresenterTests: XCTestCase {
             router: self.routerMock
         )
     }
+
+    // MARK: - Test Methods
+
+    // MARK: MonsterDetailEventHandler
+
+    // MARK: viewDidLoad()
+
+    @Test
+    func viewDidLoad() {
+        presenter.viewDidLoad()
+    }
+    
+    // MARK: didTapDancingImageView()
+    
+    @Test
+    func didTapDancingImageView_notNil() {
+        presenter.didTapDancingImageView(dancingImage: UIImage())
+        
+        #expect(routerMock.popupDancingImageCallCount == 1)
+    }
+    
+    @Test
+    func didTapDancingImageView_nil() {
+        presenter.didTapDancingImageView(dancingImage: nil)
+        
+        #expect(routerMock.popupDancingImageCallCount == 0)
+    }
+    
+    // MARK: didTapShareButton()
+    
+    // FIXME: A build error occurs.
+    // ref: https://github.com/apple/swift-testing/issues/41
+//    struct DidTapShareButtonOneNilArgument {
+//        let senderView: UIView?
+//        let name: String?
+//        let description: String?
+//        let icon: UIImage?
+//    }
+//    @Test(arguments: [
+//        DidTapShareButtonOneNilArgument(senderView: nil, name: "name", description: "description", icon: UIImage()),
+//        DidTapShareButtonOneNilArgument(senderView: UIView(), name: nil, description: "description", icon: UIImage()),
+//        DidTapShareButtonOneNilArgument(senderView: UIView(), name: "name", description: nil, icon: UIImage()),
+//        DidTapShareButtonOneNilArgument(senderView: UIView(), name: "name", description: "description", icon: nil),
+//    ])
+//    func didTapShareButton_one_nil(_ argument: DidTapShareButtonOneNilArgument) {
+//        presenter.didTapShareButton(argument.senderView, name: argument.name, description: argument.description, icon: argument.icon)
+//        #expect(routerMock.showActivityCallCount == 0)
+//    }
+    
+    @Test
+    func didTapShareButton_all_notNil() {
+        let senderView = UIView()
+        let name = "name"
+        let description = "description"
+        let icon = UIImage()
+        routerMock.showActivityHandler = { sourceView, text, image in
+            #expect(text == "\(name)\n\(description)\n#UhooiPicBook")
+            #expect(sourceView == senderView)
+            #expect(image == icon)
+        }
+        
+        presenter.didTapShareButton(senderView, name: name, description: description, icon: icon)
+
+        #expect(routerMock.showActivityCallCount == 1)
+    }
+
+    // MARK: MonsterDetailInteractorOutput
 }
